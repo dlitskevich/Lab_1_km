428e34ab8c39f82092af529e7c05464f312927d8
Danila <danila.litskevich@gmail.com>
2019-03-17 14:04:27 +0300

    Done: All combs

--- 21_lab_1_2.py
+++ 21_lab_1_2.py
@@ -1,3 +1,4 @@
+import sys
 
 
 def unique_words(sentence):
@@ -11,6 +12,26 @@ def unique_words(sentence):
     return unique_list
 
 
+def add_word_combinations(combinations, new_word, order):
+    """
+    to all elements add new word and place it in suitable position
+    :param combinations:
+    :param new_word:
+    :param order:
+    :return: list of all combinations with current words
+    """
+    combinations_list = combinations
+    combinations_list.append([])
+
+    for row_old in range(order, 0, -1):
+        for each_old in range(len(combinations_list[row_old - 1])):
+            combinations_list[row_old].append(
+                combinations_list[row_old - 1][each_old] + new_word
+            )
+    combinations_list[0].append(new_word)
+    return combinations_list
+
+
 def combinations(words):
     """
     combine words
@@ -22,21 +43,12 @@ def combinations(words):
 
     for row in range(0, quantity):
         new_word = [words[row]]
-        combinations_list.append([])
-        # to all elements add new word and place it in suitable position
-        for row_old in range(row, 0, -1):
-            for each_old in range(len(combinations_list[row_old-1])):
-                combinations_list[row_old].append(
-                    combinations_list[row_old-1][each_old] + new_word
-                )
-        combinations_list[0].append(new_word)
-        # to see how it works step by step
-        # print(combinations_list)
+        combinations_list = \
+            add_word_combinations(combinations_list, new_word, row)
         """
-        for word_2 in words[i:]:
-            combinations_list.append(word_1 + word_2)
+        to see how it works step by step
+        print(combinations_list)
         """
-
     return combinations_list
 
 
@@ -54,11 +66,18 @@ def even_combinations(words):
 
 
 if __name__ == "__main__":
-    txt = "A B C A D A"
 
-    print(unique_words(txt))
+    if len(sys.argv) >= 2:
+        text = (sys.argv[1])
+    else:
+        text = (input("Input text: "))
+
+    print(unique_words(text))
+    print(even_combinations(unique_words(text)))
+
+    """ Testament
+    # text = "A B C A D A E"
     # print(combinations(unique_words(txt)))
-    print(even_combinations(unique_words(txt)))
-
     # print(combinations(["A", "B", "C", "D", "E"]))
     # print(even_combinations(["A", "B", "C", "D", "E"]))
+    """
