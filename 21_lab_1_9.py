b4ef88e8849817139dd2b9eea1d80f51efdd7787
Danila <danila.litskevich@gmail.com>
2019-03-18 12:27:48 +0300

    TODO: better catch exception

--- 21_lab_1_9.py
+++ 21_lab_1_9.py
@@ -27,7 +27,6 @@ def subflatten(nested_list):
             for element in subflatten(nested_list[index]):
                 yield element
     except TypeError:
-        pass
         yield nested_list
 
 
@@ -42,6 +41,10 @@ if __name__ == "__main__":
     print(a)
     print(task(a))
 
+    nested_list_test = [([1, (2), 3], (4, 5), [[6], (7, 8)], 9)]
+    print(nested_list_test)
+    print(task(nested_list_test))
+    
     # let input from command line
     if len(sys.argv) >= 2:
         nested = eval(sys.argv[1])
