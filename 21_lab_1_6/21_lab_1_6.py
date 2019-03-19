a541e05553bb38d782bdf9fc0706d927973d981a
Danila <danila.litskevich@gmail.com>
2019-03-19 18:08:44 +0300

    Done

--- 21_lab_1_6/21_lab_1_6.py
+++ 21_lab_1_6/21_lab_1_6.py
@@ -3,8 +3,35 @@ import getopt
 import random
 
 
-def generate_garbage(rows, quantity, length=random.randrange(1, 16)):
-    """"""
+def random_syllable():
+    """
+    random syllable in range 33 - 126 (can be more)
+    :return: char
+    """
+    return chr(random.randrange(33, 126))
+
+
+def garbage_word(length):
+    """
+    create word from random syllables
+    :param length:
+    :return: word
+    """
+    word = ""
+    for syllable in range(length):
+        word += random_syllable()
+    return word
+
+
+def generate_garbage(file, rows, quantity, length=random.randrange(1, 16)):
+    """
+    fill file with garbage
+    :param file: where to write
+    :param rows:
+    :param quantity: of words in a row
+    :param length: of each word
+    :return: None
+    """
     try:
         rows, quantity, length = \
             int(rows), int(quantity), int(length)
@@ -14,13 +41,26 @@ def generate_garbage(rows, quantity, length=random.randrange(1, 16)):
     assert quantity > 0 and rows > 0 and length > 0, \
         "rows, quantity, length must be positive int"
 
-    print(rows, quantity, length)
+    with open(file, "w") as output:
+        """
+        quite many chars
+        for i in range(1000):
+            output.write("{}".format(i) + ": " + chr(i) + "\n")
+        """
+        for row in range(rows):
+            for word in range(quantity):
+                output.write(garbage_word(length) + "   ")
+            output.write("\n")
+
+    print(" Rows: {0} \n"
+          " Quantity of words: {1} \n"
+          " length of every word: {2}".format(rows, quantity, length))
 
 
 if __name__ == "__main__":
 
     argv = sys.argv[1:]
-    outputfile = 'output.txt'
+    outputfile = "output.txt"
 
     try:
         opts, args = getopt.getopt(argv, "ho:", ["ofile="])
@@ -34,6 +74,6 @@ if __name__ == "__main__":
         elif opt in ("-o", "--ofile"):
             outputfile = arg
 
-    print('Output file is ', outputfile)
+    print(" Output file is ", outputfile)
 
-    generate_garbage(*args[:3])
+    generate_garbage(outputfile, *args[:3])
