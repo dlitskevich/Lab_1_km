675ff958aa1ad0368e55b4e229ed7eba0cf05316
Danila <danila.litskevich@gmail.com>
2019-03-20 18:59:43 +0300

    TODO: merge_two

--- 21_lab_1_5/21_lab_1_5.py
+++ 21_lab_1_5/21_lab_1_5.py
@@ -1,17 +1,37 @@
 import sys
 import getopt
-import random
 
 
-def merge_sort(*args):
+def merge_two(unsorted_list):
     """"""
+    length = len(unsorted_list) // 2
+    first_part = unsorted_list[:length]
+    second_part = unsorted_list[length:]
+    if length == 0:
+        yield unsorted_list
+    else:
+        for i in merge_two(first_part):
+            yield i
+        for el in merge_two(second_part):
+            yield el
+
+    # print(first_part, second_part)
+
+
+def merge_sort(file, unsorted_list):
+    """"""
+    for step in merge_two(unsorted_list):
+        print(step)
+
     return None
-def task(file, *arguments):
+
+
+def task(file, unsorted_list):
     """"""
     if file:
-        merge_sort(file, *arguments[:3])
+        merge_sort(file, unsorted_list)
     else:
-        print(merge_sort(file, *arguments[:3]))
+        merge_sort(file, unsorted_list)
 
 
 if __name__ == "__main__":
@@ -36,4 +56,5 @@ if __name__ == "__main__":
     else:
         print(" Output in console: \n")
 
-    task(outputfile, *args[:3])
+    list = [13, 4, 10, 6, 15, 1, 12, 7, 9, 16, 3, 11, 5, 8, 2, 14]
+    task(outputfile, list)
